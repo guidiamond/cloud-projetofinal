454610b9a4c0c7d06237ea40cad09e175a4f3255
guidiamond <guilherme.batista770@gmail.com>
2020-12-01 13:35:25 -0300

    loadbalencer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError
+from time import time
 
 postgress_script = """#!/bin/bash
          sudo apt update
@@ -81,6 +82,42 @@ oregon = {
                 "IpProtocol": "tcp",
                 "FromPort": 8080,
                 "ToPort": 8080,
+                "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+            },
+        ],
+    },
+}
+elb_client = boto3.client("elb", region_name=oregon_region)
+elb = {
+    "region": oregon_region,
+    "name": "lb",
+    "ami": {"id": None},
+    "img_id": "ami-0dd9f0e7df0f0a138",  # Ubuntu 18.04
+    "client": elb_client,
+    "key": "oregon-key",
+    "resource": boto3.resource("ec2", region_name=oregon_region),
+    "instance": {
+        "tag": {"Key": "Name", "Value": "instance_loadbalencer"},
+        "id": None,  # Reasigned in create_instance
+        "ip": None,  # Reasigned in create_instance
+    },
+    "script": postgress_script,
+    "vpc": ohio_client.describe_vpcs().get("Vpcs", [{}])[0].get("VpcId", ""),
+    "security_group": {
+        "name": "postgres",
+        "id": None,
+        "tag": {"Key": "Name", "Value": "security_guilhermetb"},
+        "value": [
+            {
+                "IpProtocol": "tcp",
+                "FromPort": 22,
+                "ToPort": 22,
+                "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
+            },
+            {
+                "IpProtocol": "tcp",
+                "FromPort": 5432,
+                "ToPort": 5432,
                 "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
             },
         ],
@@ -224,28 +261,51 @@ def delete_instances(obj):
         print("Error", e)
         return False
 
-     def create_load_balancer_v2(self, name, security_group, tag):
-          print("\nCreating load_balancer name=%s" % (name))
-          try:
-               waiter = self.elvb2_client.get_waiter('load_balancer_available')
-               load_balancer_arn = self.elvb2_client.create_load_balancer(
-                    Name=name,
-                    Subnets=self.subnets,
-                    Tags=[tag],
-                    Type='classic',
-               )['LoadBalancers'][0]['LoadBalancerArn']
 
-               waiter.wait(LoadBalancerArns=[load_balancer_arn])
-               response = self.elvb2_client.create_listener(
-                    DefaultActions=[{'TargetGroupArn': tg_arn, 'Type': 'forward'}],
-                    LoadBalancerArn=load_balancer_arn,
-                    Port=80,
-                    Protocol='HTTP',
-               )
-               print("load_balancer arn=%s created" % (load_balancer_arn))
+def get_timer(time_s):
+    t0 = time()
+    t1 = time()
+    while t1 - t0 <= time_s:
+        t1 = time()
 
-          except ClientError as e:
-               print('Error', e)
+
+def create_load_balancer(obj, subnets, security_group_id):
+    print("\nCreating load_balancer")
+    try:
+
+        load_balancer = obj["client"].create_load_balancer(
+            LoadBalancerName=obj["name"],
+            Listeners=[
+                {
+                    "Protocol": "HTTP",
+                    "LoadBalancerPort": 8080,
+                    "InstancePort": 8080,
+                },
+            ],
+            Subnets=subnets,
+            SecurityGroups=[security_group_id],
+            Tags=[
+                {"Key": "string", "Value": "string"},
+            ],
+        )
+
+        with open("loadbalancer_DNS", "w+") as f:
+            f.write(load_balancer["DNSName"])
+
+        ok = False
+        while not ok:
+            lb = obj["client"].describe_load_balancers()["LoadBalancerDescriptions"]
+            for l in lb:
+                if l["LoadBalancerName"] == obj["name"]:
+                    ok = True
+
+            get_timer(10)
+
+        print("load_balancer created")
+
+    except ClientError as e:
+        print("Error", e)
+
 
 def main():
     # ohio
@@ -257,9 +317,16 @@ def main():
     create_security_group(oregon)  # assign oregon's security_group id
     create_instance(oregon)  # assign oregon's instance ip and id
 
+    # get subnets used for the load balencer
+    subnets = [
+        subnet["SubnetId"] for subnet in oregon["client"].describe_subnets()["Subnets"]
+    ]
+
     # Create AMI and delete oregon instance
     create_ami(oregon)
     delete_instances(oregon)
+
+    create_load_balancer(elb, subnets, oregon["security_group"]["id"])
 
 
 main()
